47dce6d22035524ba26e4f1c12f6210c4e960b5c
nico <nicolasmgarcia0.gmail.com>
2024-10-14 19:23:47 -0300

    owner entity added

---
+++ TPI Proyecto Programacion 3/Domain/Entities/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Owner : User
+    {
+        public List<Building> Property { get; set; } = new List<Building>();
+        public string? Photo { get; set; }
+        public int? Rating {get; set;}
+    }
+}
--- TPI Proyecto Programacion 3/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ TPI Proyecto Programacion 3/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385908c0709729b3d3235504b816ff90be5a4814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe7950ddec160882946bf97cbb6fabd865cc1cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
